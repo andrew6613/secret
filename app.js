5cd79d050ab5d201a645a1d6d68d6d1917ef3074
andrew6613 <92650857+andrew6613@users.noreply.github.com>
2023-01-08 19:50:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let button1 = document.getElementById("1");
+let button2 = document.getElementById("2");
+let button3 = document.getElementById("3");
+button1.addEventListener("click", () => {
+  alert("Урррррраааа!!!!!");
+});
+button2.addEventListener("click", () => {
+  alert("Урррррраааа!!!!!");
+});
+button3.addEventListener("click", () => {
+  alert("Придётся подождать");
+});
